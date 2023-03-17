265786238e3af13729db7f9103a3f64d65cda4cf
Lenoix <61371995+Lenoix@users.noreply.github.com>
2023-03-17 14:30:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,10 @@
 
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
+echo 'src-git passwall_package https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-new-version' >>feeds.conf.default
+
 #sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
 
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
